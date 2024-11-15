e7bba53a8d5a6fe7771dd0dbbcdd9fab49eb865e
Viktor <28753370+kirillovmr@users.noreply.github.com>
2024-11-15 15:37:36 -0700

    Updates

---
+++ assets/index-BuIFHMbC.js
@@ -0,0 +1,2 @@
+import{d as a,b as i,r as n,j as e}from"./index-Tg-VYr3K.js";import{u as o,S as u}from"./_layout-ol0vyv8L.js";function d(){const{db:t}=a(o(s=>({db:s.db}))),r=i();return n.useEffect(()=>{if(!t)return r("/")},[t]),t?e.jsx(u,{children:e.jsx(e.Fragment,{children:e.jsx("p",{children:"👈 Select table"})})}):e.jsx(e.Fragment,{})}export{d as default};
+//# sourceMappingURL=index-BuIFHMbC.js.map
