26656fd978405eb20b75f62955067e1c6369285c
Viktor <28753370+kirillovmr@users.noreply.github.com>
2024-11-15 15:21:31 -0700

    Updates

--- assets/index-BG_il4xh.js
+++
@@ -1,2 +0,0 @@
-import{d as a,b as i,r as n,j as e}from"./index-ysujwTdI.js";import{u as o,S as u}from"./_layout-CWRW7eiL.js";function d(){const{db:t}=a(o(s=>({db:s.db}))),r=i();return n.useEffect(()=>{if(!t)return r("/")},[t]),t?e.jsx(u,{children:e.jsx(e.Fragment,{children:e.jsx("p",{children:"👈 Select table"})})}):e.jsx(e.Fragment,{})}export{d as default};
-//# sourceMappingURL=index-BG_il4xh.js.map
