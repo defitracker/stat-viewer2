28b4596a05032f7b81f13714b1574a1c29295293
Viktor <28753370+kirillovmr@users.noreply.github.com>
2024-11-15 15:01:51 -0700

    Updates

---
+++ assets/index-BuSpjXm0.js
@@ -0,0 +1,2 @@
+import{d as a,b as i,r as n,j as e}from"./index-C-eJAOgP.js";import{u as o,S as u}from"./_layout-do5jNA5D.js";function d(){const{db:t}=a(o(s=>({db:s.db}))),r=i();return n.useEffect(()=>{if(!t)return r("/")},[t]),t?e.jsx(u,{children:e.jsx(e.Fragment,{children:e.jsx("p",{children:"👈 Select table"})})}):e.jsx(e.Fragment,{})}export{d as default};
+//# sourceMappingURL=index-BuSpjXm0.js.map
